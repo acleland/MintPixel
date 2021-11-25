60dc2466810341a1871bd13e99c5694165036667
Andrea <andreaclelan@gmail.com>
2021-11-25 08:33:47 -0800

    did some restyling

--- draw.js
+++ draw.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext('2d');
 
-canvas.width = 500;
-canvas.height = 250;
+canvas.width = 700;
+canvas.height = 700;
 
 
 let canvasStates = []
